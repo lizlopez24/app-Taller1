5a09f7a6eb62251b732a0cedc5db44d34e732215
Pao2206 <paolatm22@hotmail.com>
2024-01-05 18:02:51 -0500

    Conexion con Firebase
    Conexion y creacion de un usuario para prueba

---
+++ config/Config.tsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database'; 
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYMNn2v2FSYPH0VO_k75DK_Nco1YiQzeg",
+  authDomain: "app-taller1.firebaseapp.com",
+  databaseURL: "https://app-taller1-default-rtdb.firebaseio.com",
+  projectId: "app-taller1",
+  storageBucket: "app-taller1.appspot.com",
+  messagingSenderId: "884196235709",
+  appId: "1:884196235709:web:cacd26600c648bb9119ad4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app)
--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,56 +1,103 @@
-import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { TextInput } from 'react-native-gesture-handler'
+import {
+  Alert,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
+
+import { db } from "../config/Config";
+import { ref, set } from "firebase/database";
 
 export default function RegisterScreen() {
+  const [correo, setCorreo] = useState("");
+  const [contrasena, setContrasena] = useState("");
+  const [usuario, SetUsuario] = useState("");
+
+  function registro(correo: string, contrasena: string, usuario: string) {
+    set(ref(db, "registros-nuevos/" + usuario), {
+      email: correo,
+      password: contrasena,
+      user: usuario,
+    });
+
+    Alert.alert("Mensaje", "Se registro tu usuario");
+  }
+
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Regístrate</Text>
-            <Image source={{ uri: 'https://e7.pngegg.com/pngimages/858/581/png-clipart-profile-icon-user-computer-icons-system-chinese-wind-title-column-miscellaneous-service.png' }} style={styles.img} />
-            <TextInput placeholder='Ingrese su correo' style={styles.input} />
-            <TextInput placeholder='Ingrese su nombre de usuario' style={styles.input} />
-            <TextInput placeholder='Ingrese su contraseña' style={styles.input}/>
-            <TextInput placeholder='Confirme su contraseña' style={styles.input} />
-            <Pressable style={styles.btn}>
-                <Text>Registrar</Text>
-            </Pressable>
-            
+      <Text style={styles.title}>Regístrate</Text>
+      <Image
+        source={{
+          uri: "https://e7.pngegg.com/pngimages/858/581/png-clipart-profile-icon-user-computer-icons-system-chinese-wind-title-column-miscellaneous-service.png",
+        }}
+        style={styles.img}
+      />
+      <TextInput
+        placeholder="Ingrese su correo"
+        onChangeText={(texto) => setCorreo(texto)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Ingrese su nombre de usuario"
+        onChangeText={(texto) => SetUsuario(texto)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Ingrese su contraseña"
+        onChangeText={(texto) => setContrasena(texto)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Confirme su contraseña"
+        onChangeText={(texto) => setContrasena(texto)}
+        style={styles.input}
+      />
+      <Pressable
+        style={styles.btn}
+        onPress={() => registro(correo, contrasena, usuario)}
+      >
+        <Text>Registrar</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 30,
-        marginTop: 60,
-        marginBottom: 30
-
-    },
-    btn: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: '#80d3ec',
-        marginVertical:30
-    },
-    input: {
-        width: 275,
-        marginVertical: 20,
-        backgroundColor: 'white',
-        color: 'black',
-        borderRadius: 5,
-        paddingStart: 15
-    },
-    img: {
-        width: 150,
-        height: 150,
-        resizeMode: 'contain'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 60,
+    marginBottom: 30,
+  },
+  btn: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#80d3ec",
+    marginVertical: 30,
+  },
+  input: {
+    width: 275,
+    marginVertical: 20,
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: 5,
+    paddingStart: 15,
+  },
+  img: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+  },
+});
