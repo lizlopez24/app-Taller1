c1384c1db31a1cb62e9fac3628b63dd39749078c
lizlopez24 <133879942+lizlopez24@users.noreply.github.com>
2024-01-09 10:24:24 -0500

    config

--- config/Config.tsx
+++ config/Config.tsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database'; 
-import { getAuth } from "firebase/auth";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +17,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app)
-export const auth=getAuth(app)
