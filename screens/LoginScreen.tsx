fe43b5e0eb2a733b9fd6ec36dabc32cf9fc9ef1d
lizlopez24 <133879942+lizlopez24@users.noreply.github.com>
2024-01-09 10:11:01 -0500

    login con autenticacion

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,19 +1,45 @@
 import { Button, Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
-import RegisterScreen from './RegisterScreen'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../config/Config'
 
 export default function LoginScreen({ navigation }: any) {
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+
+    function login(){
+        signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        //Accede a la navegacion
+        navigation.navigate('Tab')
+        setemail('')
+        setpassword('')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+      }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Iniciar Sesión</Text>
             <Image source={{ uri: 'https://e7.pngegg.com/pngimages/858/581/png-clipart-profile-icon-user-computer-icons-system-chinese-wind-title-column-miscellaneous-service.png' }} style={styles.img} />
-            <TextInput placeholder='Ingrese su nombre de usuario' style={styles.input} />
-            <TextInput placeholder='Ingrese su contraseña' style={styles.input}/>
-            <Pressable onPress={() => navigation.navigate('Tab')}
+            <TextInput placeholder='Ingrese su nombre de usuario' style={styles.input} 
+            keyboardType='email-address'
+            onChangeText={(texto)=>setemail(texto)}
+            value={email}/>
+            <TextInput placeholder='Ingrese su contraseña' style={styles.input}
+            onChangeText={(texto)=>setpassword(texto)}
+            value={password}/>
+
+            <Pressable onPress={() => login()}
                 style={styles.btn}>
                 <Text>Iniciar Sesion</Text>
             </Pressable>
+
             <Text>¿Aún no tienes cuenta?</Text>
             <TouchableOpacity onPress={() => navigation.navigate('Registrarse')}>
                 <Text style={styles.txt}>Regístrate!</Text>
